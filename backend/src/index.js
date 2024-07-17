b433b99900b249cf2568244534aee49b5e57c2ea
Ivan Krug <139096035+IvanKrug@users.noreply.github.com>
2024-07-17 13:12:12 -0400

    Github actions workflow

--- backend/src/index.js
+++
@@ -1,6 +0,0 @@
-import app from './app.js';
-import 'dotenv/config'
-import { connectDB } from './db.js';
-connectDB();
-app.listen(process.env.PORT);
-console.log('server en puerto ', process.env.PORT);
